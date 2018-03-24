29b4afbe576eaff7bab49b64e69788fed0d652df
Ainsey11 <ainsey11@users.noreply.github.com>
2018-03-24 02:32:36 +0000

    idk what happened here, had a drinkypoo

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,28 +1,42 @@
 #include <SPI.h>
 #include <Ethernet.h>
+#include <LiquidCrystal.h>
+#include <Wire.h>
 
-//Mac Address - Random Made one
-byte mac[] = {  
-  0xBE, 0xEF, 0x00, 0x00, 0xDE, 0x02 };
 
-//Init Ethernet client lib
+// Random Mac address
+byte mac[] = {
+  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02
+};
+
+//Start Ethernet client up
 EthernetClient client;
 
+//Init LCD1
+LiquidCrystal lcd(0x3F);
+
+
+
 void setup() {
-  //start serial port up
+  // start the serial library:
   Serial.begin(9600);
-  //start ethernet connection
+  
+  // start the Ethernet connection:
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
     // no point in carrying on, so do nothing forevermore:
-    for(;;)
+    for (;;)
       ;
+  }
+  // print your local IP address:
+  Serial.println(Ethernet.localIP());
+
+  // LCD
+  lcd.begin(16,2);
+  lcd.clear();
+  lcd.print("Test1");
 }
 
-Serial.println(Ethernet.localIP());
-
-
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
