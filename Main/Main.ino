5ad7ca7c02515e1bf16982c80e2b8b5f29b587a2
Ainsey11 <ainsey11@users.noreply.github.com>
2018-03-24 01:14:27 +0000

    First commit, adding MAC address and init of client lib

---
+++ Main/Main.ino
@@ -0,0 +1,19 @@
+#include <SPI.h>
+#include <Ethernet.h>
+
+//Mac Address - Random Made one
+byte mac[] = {  
+  0xBE, 0xEF, 0x00, 0x00, 0xDE, 0x02 };
+
+//Init Ethernet client lib
+EthernetClient client;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
