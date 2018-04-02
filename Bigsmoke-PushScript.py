fc6f020733f546986194beb1be4ec0c18c2f9460
Ainsey11 <robert@ainsey11.com>
2018-04-02 02:37:48 +0100

    moved openhab open back and tweaked formatting for the condition output so it reads better

--- Bigsmoke-PushScript.py
+++ Bigsmoke-PushScript.py
@@ -12,7 +12,6 @@ OpenHabURL = "http://172.16.2.41:8880/rest"
 # Send startup message to show big smoke has launched the 
 # script up
 
-openhab = openHAB(OpenHabURL) # Start the openhab connection / lib up as slow asf
 
 StartL1 = "Bigsmoke"
 StartL2 = "Has+Started"
@@ -33,11 +32,12 @@ while True:
 	time.sleep(30)
 	
 	# Outside temp from OpenHAB
+	openhab = openHAB(OpenHabURL)
 	tempreq = openhab.get_item('weatherunderground_weather_9d9f927c_current_feelingTemperature')
 	temp = str(float(tempreq.state))
 	conditionreq = openhab.get_item('weatherunderground_weather_9d9f927c_current_conditions')
 	condition = str(conditionreq.state)
-	openhabconnection =  urllib2.urlopen('http://' + ArduinoIP + '?L1=Outdoor+Conditions:' + '&L2=' + temp + 'C' + condition)
+	openhabconnection =  urllib2.urlopen('http://' + ArduinoIP + '?L1=Outdoor+Conditions:' + '&L2=' + temp + 'C' + ' / ' + condition)
 	openhabconnection.close()
 	print("Sent Temp to Arduino")
 	time.sleep(30)
