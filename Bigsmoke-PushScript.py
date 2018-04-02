bc50c3ecc060c3ce419e96432ae6a30076ad27f7
Ainsey11 <robert@ainsey11.com>
2018-04-02 01:45:11 +0100

    Added basic variables, swapped back to vim for dev, pycharm sucks

--- Bigsmoke-PushScript.py
+++ Bigsmoke-PushScript.py
@@ -1,3 +1,19 @@
-# Set up the Arduino IP address
+import urllib2
+import time
+from datetime import datetime
 
+
+# Set up the Arduino IP address
 ArduinoIP = "172.16.2.44"
+
+
+# Send startup message to show big smoke has launched the 
+# script up
+
+StartL1 = "Bigsmoke"
+StartL2 = "Has+Started"
+
+urllib2.urlopen('http://' + ArduinoIP + '?L1=' + StartL1 + '&L2=' + StartL2)
+
+
+
