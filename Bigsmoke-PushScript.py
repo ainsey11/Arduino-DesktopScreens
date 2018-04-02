6cd4ddc0a11899cba3617b107e863c9b4c050157
Ainsey11 <robert@ainsey11.com>
2018-04-02 02:36:41 +0100

    changed temp to temp and outside condition

--- Bigsmoke-PushScript.py
+++ Bigsmoke-PushScript.py
@@ -12,6 +12,8 @@ OpenHabURL = "http://172.16.2.41:8880/rest"
 # Send startup message to show big smoke has launched the 
 # script up
 
+openhab = openHAB(OpenHabURL) # Start the openhab connection / lib up as slow asf
+
 StartL1 = "Bigsmoke"
 StartL2 = "Has+Started"
 
@@ -27,9 +29,15 @@ while True:
 	Time = datetime.now().strftime('%H:%M:%S')
 	timeconnection = urllib2.urlopen('http://' + ArduinoIP + '?L1=Time+Is:' + '&L2=' + Time)
 	timeconnection.close()
-	time.sleep(5)
+	print("Sent Time to Arduino")
+	time.sleep(30)
 	
 	# Outside temp from OpenHAB
-	openhab = openHAB(OpenHabURL)
-	item = openhab.get_item('weatherunderground_weather_9d9f927c_current_feelingTemperature')
-	print item.state
+	tempreq = openhab.get_item('weatherunderground_weather_9d9f927c_current_feelingTemperature')
+	temp = str(float(tempreq.state))
+	conditionreq = openhab.get_item('weatherunderground_weather_9d9f927c_current_conditions')
+	condition = str(conditionreq.state)
+	openhabconnection =  urllib2.urlopen('http://' + ArduinoIP + '?L1=Outdoor+Conditions:' + '&L2=' + temp + 'C' + condition)
+	openhabconnection.close()
+	print("Sent Temp to Arduino")
+	time.sleep(30)
